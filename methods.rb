b7b6d06ef6501ef30721551a42e68b23c6e5a8ee
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-06 11:22:48 +0300

    passed all tests

--- methods.rb
+++ methods.rb
@@ -1,1 +1,39 @@
 # Your code here!
+
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+def add (a,b)
+    a+b
+end
+
+add(3,4)
+
+
+
+
+def halve(number)
+    if number.is_a?(Integer)
+      number / 2
+    else
+      nil
+    end
+end
+
+  halve(6)
